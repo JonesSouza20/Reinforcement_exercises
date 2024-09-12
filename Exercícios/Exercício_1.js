5e293b95eb85bdd2bcd77cb8e0cc024cfc642274
JonesSouza20 <jonessouza1999@gmail.com>
2024-09-12 12:16:41 -0300

    Exercício 1

---
+++ Exercícios/Exercício_1.js
@@ -0,0 +1,34 @@
+//Declaração de variaveis
+
+var nome = "Jones";
+
+let idade = 25;
+
+const mesAno = 9.2024;
+
+console.log (nome, idade, mesAno);
+
+//Conversão dos tipos de  dados
+
+let texto = "Jones";
+
+let ano = 1999;
+
+console.log(texto, ano);
+
+texto = 25;
+
+ano = "Nascimento no dia 20 de Agosto"
+
+console.log(texto, ano);
+
+//Operações aritméticas
+
+let a = 12
+let b = 57
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b)
