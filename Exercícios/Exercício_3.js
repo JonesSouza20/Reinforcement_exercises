6bbceb7ba102fa359e4e1e90b96af9d236740db6
JonesSouza20 <jonessouza1999@gmail.com>
2024-09-16 20:53:26 -0300

    Aula 3 exercício de reforço

---
+++ Exercícios/Exercício_3.js
@@ -0,0 +1,19 @@
+let teste = [10, 20, 30];
+
+for(let contador = 0; contador < teste.length; contador++ ){
+    console.log(teste[contador])
+};
+
+let teste2 = 0
+
+while (teste2 < teste.length){
+    console.log(teste[teste2]);
+    teste2++
+}
+
+let teste3 = 0
+
+do {
+    console.log(teste[teste3]);
+    teste3++
+} while (teste3 < teste.length);
