5f6bc069be4e21fc419ca548c2639cec32d768b0
JonesSouza20 <jonessouza1999@gmail.com>
2024-09-20 18:41:35 -0300

    Exercico de reforço 6

---
+++ Exercícios/Exercício_6.js
@@ -0,0 +1,28 @@
+//Exercício para definição e manipulação de objeto
+
+//Criação do objeto
+
+let funcionario = {
+    nome: "José Carlos",
+    idade: 40,
+    cargo: "Porteiro",
+    salario: 2600
+};
+
+console.log(funcionario);
+
+//Alterando propriedades do objeto
+
+funcionario.salario = 3000;
+
+funcionario.cargo = "Supervisor";
+
+console.log(funcionario);
+
+//Adicionando propriedade no objeto
+
+funcionario.localidade = "São Paulo";
+
+console.log(funcionario)
+
+//Fim do código :D
