ed705bb0a8f79ef5831ea8c1cfd7e5c401f6488b
JonesSouza20 <jonessouza1999@gmail.com>
2024-10-07 17:16:02 -0300

    Observações

--- Exercícios/Exercício_13.js
+++ Exercícios/Exercício_13.js
@@ -1,14 +1,17 @@
-function contador(contar) {
-    let numeros = 0;
+//Manipulando closures em funções
 
+function contador(contar) { //Primeira função.
+    let numeros = 0; //Declaração de variavel para a função, a declaração foi feita com let pois permite a alteração de sua variavel.
+
+ //Chamando a segunda função com base na variavel declarada na primeira. Closures permite que esta função "lembre-se" dos parametros da primeira função.
         return function() {
-            numeros++;
+            numeros++; //Contagem dos números começando a partir do zero, informado na variavel declarada anteriormente.
             console.log(numeros)
         };
 };
 
 
-const contagem = contador();
+const contagem = contador(); //Declaração de uma nova variavel para armazenar os valores da função
 
 contagem();
 contagem();
--- Exercícios/Exercício_14.js
+++ Exercícios/Exercício_14.js
@@ -1,13 +1,15 @@
-async function dadosAPI() {
-    let resposta = await fetch('https://jsonplaceholder.typicode.com/posts');
-    let resultado = await resposta.json();
+//Manipulação de async e await para puxar dados de uma API
+
+async function dadosAPI() { //Declaração da função
+    let resposta = await fetch('https://jsonplaceholder.typicode.com/posts'); //Declaração de uma variavel chamando os dados da API com o link da mesma.
+    let resultado = await resposta.json(); //Declaração de uma segunda variavel para o retorno dos dados da API.
     
-    try {
+    try { //Bloco para verificação de possiveis erros com os dados da API
         resposta
         resultado
         console.log(resultado)
     } catch(erro) {
-        console.log("Não é possível buscar os dados da API.", erro);
+        console.log("Não é possível buscar os dados da API.", erro); //Mensagem caso ocorra algum erro na verificação dos dados da API.
         
     }
 }
